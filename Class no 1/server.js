94bf61925f266ad3983e2996e1cb2ae01026f7d3
ArAhmadRaza <arahmadraza5570@gmail.com>
2024-12-16 12:38:49 +0500

    Initial commit

---
+++ Class no 1/server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+app.put('/products', (req, res) => {
+  let products = [
+    { id: 1, title: 'Product 1 by Ahmad ', price: 100, category: 'Category 1', description: 'Description 1', image: 'Image 1' },
+    { id: 2, title: 'Product 2', price: 200, category: 'Category 2', description: 'Description 2', image: 'Image 2' },
+    { id: 3, title: 'Product 3', price: 300, category: 'Category 3', description: 'Description 3', image: 'Image 3' },
+    { id: 4, title: 'Product 4', price: 400, category: 'Category 4', description: 'Description 4', image: 'Image 4' },
+]
+res.json(products)
+  })
+  
+// add - post method
+// update - put method
+// delete - delete method
+// get - get method
+
+
+app.get('/xyz', (req, res) => {
+  let users = [
+      { name: 'John', age: 25 },
+      { name: 'Jane', age: 22 },
+      { name: 'Jim', age: 32 }
+  ]
+  res.json(users)
+});
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
