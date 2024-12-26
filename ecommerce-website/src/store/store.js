bf12edeb08c2da7b931aeae2c4af4a3208d89a29
ArAhmadRaza <arahmadraza5570@gmail.com>
2024-12-26 23:50:07 +0500

    Starting Frontend

--- ecommerce-website/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import productReducer from "./slices/productSlice"
-
-
-export const store = configureStore({
-    reducer:{
-        productSlice: productReducer,  // Add your slice reducers here. For example, {product: productReducer}
-    }
-});
